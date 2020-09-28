27819fe7ef2cffa3b570ed8132e3e677675f590c
Exr0n <spotyie@gmail.com>
2020-09-27 21:20:02 -0700

    what if i want actual medium, but average time of day?

--- seedtype.py
+++ seedtype.py
@@ -1,6 +1,6 @@
-from abc import ABC, abstractmethod # https://www.python-course.eu/python3_abstract_classes.php
+from abc import ABC, abstractmethod         # https://www.python-course.eu/python3_abstract_classes.php
 from enum import Enum, auto
-from typing import Callable, Any    # https://stackoverflow.com/a/39624147/10372825
+from typing import Callable, Iterable, Any  # https://stackoverflow.com/a/39624147/10372825
 
 class AccumulationType(Enum):
     SUM         = auto()
@@ -21,8 +21,7 @@ class Seedshape:
 
     @classmethod
     @abstractmethod
-    def accumulator(self, acctype: AccumulationType
-            ) -> Callable[[Any, Any], Any]:     # TODO: the Any types should be self https://github.com/python/typing/issues/271
+    def accumule(self, acctype: AccumulationType, data: Iterable[AccumulationType]):     # TODO: the Any types should be self https://github.com/python/typing/issues/271
         '''Take two instances of self and
         return another self, the combination'''
         pass
@@ -34,15 +33,21 @@ class Time(Seedshape):
 
     @property
     def data(self):
-        return self.time
+        return self.time % 86400    # time in a day
 
+    # @classmethod
+    # def factory(self, acctype):
+    #     if acctype is AccumulationType.SUM:
+    #         raise TypeError("Cannot SUM times")
+    #     elif acctype is AccumulationType.MEDIAN:
+    #         pass # TODO: am i really going to chain elifs like this??
+    #         # also, how do you even calculate median from one thing? Do i need to pass it an entire list?
     @classmethod
-    def factory(self, acctype):
-        if acctype is AccumulationType.SUM:
-            raise TypeError("Cannot SUM times")
-        elif acctype is AccumulationType.MEDIAN:
-            pass # TODO: am i really going to chain elifs like this??
-            # also, how do you even calculate median from one thing? Do i need to pass it an entire list?
-
-
+    def accumule(self, acctype, data):
+        if acctype is AccumulationType.MEDIAN:
+            return sorted(acctype)[len(acctype)/2]  # TODO: horribly inefficient
+        elif acctype is AccumulationType.AVERAGE:
+            return sum(acctype) / len(acctype)      # TODO: also horribly inefficient
+        else
+            raise TypeError(f"Invalid operation {str(acctype)} on times")
 
