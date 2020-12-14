aa838aa9fa2f238e678b6aadaca842680e7cee4f
Exr0n <mail@exr0n.com>
2020-12-13 20:17:04 -0800

    snap: tooltips

--- server/public/index.js
+++ server/public/index.js
@@ -1,3 +1,6 @@
+//const toInclude = proj => [ 'Upkeep' ].includes(proj);
+const toInclude = proj => [ '20ISOS101', '20SOM201' ].includes(proj);
+
 const squareholder = Array.from(document.getElementsByClassName('dayrow'));
 squareholder.forEach(row => {
     for (let i=0; i<52; i++) {
@@ -11,12 +14,14 @@ fetch('toggl.tsv')
     .then(text => text.split('\n'))
     .then(arr => arr.forEach(
         row => {
-            const [id, desc, _start, end] = row.split('	');
-            if (typeof _start === 'undefined') return;
+            const [id, desc, proj, _start, end] = row.split('	');
+            if (typeof _start === 'undefined' || typeof proj === 'undefined') return;
+            if (!toInclude(proj.slice(0, -13))) return;
             const start = moment(_start, 'hh:mm:ss A MM/DD/YY');
+            //console.log(desc, start);
             const square = squareholder[start.day()].children[start.week()];
             //console.log(squareholder[moment(start).day()].children[moment(start).week()]);
-            square.style = 'border: 1px solid blue;';
+            square.style = 'background-color: green;';
             //weekday.insertBefore(square, weekday.childNodes[0]);
             //console.log(id, desc, moment(start), moment(end));
         }
