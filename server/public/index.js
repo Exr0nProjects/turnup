3e6a7bd2a745a7d8a72083a71463b6734c59e4b9
Exr0n <mail@exr0n.com>
2020-12-14 15:38:56 -0800

    snap: attempt at gradient colors brick

--- server/public/index.js
+++ server/public/index.js
@@ -8,7 +8,7 @@ const toInclude = [
     //(desc, proj) => desc.toLowerCase().includes('sync') && desc.toLowerCase().includes('-'),
     //(desc, proj) => desc.toLowerCase().includes('dgg'),
     //(desc, proj) => desc.toLowerCase().includes('amon'),
-    //(desc, proj) => desc.toLowerCase().includes('vidya'),
+    (desc, proj) => desc.toLowerCase().includes('vidya'),
     //(desc, proj) => desc.toLowerCase().includes('+'),
     //(desc, proj) => desc.toLowerCase().includes('bik') && !desc.toLowerCase().includes('upkeep'),
     //(desc, proj) => desc.toLowerCase().includes('chitchat'),
@@ -30,8 +30,10 @@ const squareholder = Array.from(document.getElementsByClassName('dayrow'));
             square.setAttribute('class', 'square');
             const tooltip = document.createElement('span');
             tooltip.setAttribute('class', 'square-tooltip');
+            tooltip.style.backgroundColor = '#303030';
             tooltip.innerHTML = moment().dayOfYear(7*i+ridx-offset).format('DD MMM YYYY');
             square.tooltip = tooltip;   // hecka hacky
+            square.timesum = [0];
             square.appendChild(tooltip);
             // append
             frag.appendChild(square);
@@ -39,25 +41,44 @@ const squareholder = Array.from(document.getElementsByClassName('dayrow'));
         row.appendChild(frag);
     });
 })();
+var norm_max = 0;
+const setColor = (square, frac) => {
+    //console.log(square.style.backgroundColor)
+    //let pre = parseInt(square.style.color.slice(1), 16);
+    //console.log(square.style.color, pre, pre.toString(16));
+    //pre -= (pre % 255) * 256;
+};
 fetch('toggl2.tsv')
     .then(res => res.text())    // TODO: stream processing
     .then(text => text.split('\n'))
     .then(arr => arr.forEach(
         row => {
+            const aidx = 0;
+            // data proc
             const [id, desc, proj, _start, end] = row.split('	');
             if (typeof _start === 'undefined' || typeof proj === 'undefined') return;
-            if (!toInclude[0](desc, proj.slice(0, -13))) return;
+            if (!toInclude[aidx](desc, proj.slice(0, -13))) return;
             const start = moment(_start, 'hh:mm:ss A MM/DD/YY');
-            const square = squareholder[start.day()].children[start.week()-1];
-            square.classList.add('activated');
             const dura = moment.duration(moment(end, 'hh:mm:ss A MM/DD/YY').diff(start));
+            // dom  manip
+            const square = squareholder[start.day()].children[start.week()-1];
+            //square.classList.add('activated');
             const text = `
 ${desc} for ${dura.format('hh[h] mm[m]')}
             `.trim();
             //console.log(square.tooltip);
             square.tooltip.innerHTML += '<br>' + text;
+            // normalization
+            square.timesum[aidx] += dura.asSeconds();
+            norm_max = Math.max(norm_max, square.timesum[aidx]);
         }
-    )).catch(console.err);
+    )).then(() =>
+        squareholder.forEach((row, ridx) =>
+            Array.from(row.children).forEach(square => {
+                setColor(square, square.timesum[0]/norm_max);
+            })
+        )
+    ).catch(console.err);
 
 document.getElementById('filtercode').innerHTML = toInclude.toString();
 
