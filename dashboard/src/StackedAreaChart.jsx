17ce2b2b1c5fb5d9bc9d53c54610546717939f5b
Exr0n <mail@exr0n.com>
2021-02-21 13:47:49 -0800

    finally reveresed the x-axis

--- dashboard/src/StackedAreaChart.jsx
+++ dashboard/src/StackedAreaChart.jsx
@@ -5,6 +5,7 @@ import { useRef, useEffect } from 'react';
 export default function StackedAreaChart(props) {
     const canvasRef = useRef(null);
 
+    // make sure the y axis is filled
     let opts = props.options ?? {};
     if (!opts.hasOwnProperty('scales'))
         opts.scales = {};
@@ -12,16 +13,28 @@ export default function StackedAreaChart(props) {
         opts.scales.yAxes = Array(props.data.length).fill({});
     opts.scales.yAxes = opts.scales.yAxes.map(x => Object.assign(x, { stacked: true }));
 
+    // reverse the axes
+    let labels = [...props.data.labels];
+    labels.reverse();
+
+    const datasets = props.data.datasets.map(dataset => {
+        let thing = [...dataset.data.reverse()];
+        thing.reverse();
+        return {...dataset, data: thing}
+    });
+    
+    const data = {...props.data, labels, datasets};
+
+    // create the actual chart
     useEffect(() => { // something about https://reactjs.org/docs/hooks-effect.html
         new Chart(canvasRef.current.getContext("2d"), {
             type: 'line',
-            data: props.data,
-            //options: { axisX: { reveresed: true }, title: { text: 'hi' } },
-            //options: { ...opts, scales: { ...opts.scales, xAxes: { reveresed: true } }, title: { text: 'hi' }, },
-            //options: opts,
+            data,
+            options: opts,
         });
     });
 
+    // what we actually render
     return <div className="vis-chart">
         <div className="canvas-wrapper">
             <canvas ref={canvasRef}/>
